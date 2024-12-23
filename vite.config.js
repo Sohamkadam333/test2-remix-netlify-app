7d9251fa9cae3dace66de60a65e875c7604e0b63
soham kadam <sohamkadam58@gmail.com>
2024-12-24 00:06:35 +0530

    2 commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-  ],
+	plugins: [
+		remix({
+			future: {
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+				v3_singleFetch: true,
+				v3_lazyRouteDiscovery: true,
+			},
+		}),
+		netlifyPlugin(),
+	],
 });
